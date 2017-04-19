518019dd5553a2b955c4d3754861c1334d455b4b
yimingwang <673995231@qq.com>
2017-04-19 21:45:35 +0800

    get every menu

--- newsmth/newsmth/spiders/shuimuspider.py
+++ newsmth/newsmth/spiders/shuimuspider.py
@@ -13,8 +13,33 @@ class newsmthSpider(CrawlSpider):
     rooturl ="http://www.newsmth.net"
 
     start_urls = [rooturl+"/nForum/section/"+str(m) for m in range(1,10)
-                  ]
-
+                  ]+[rooturl +"/nForm/channel/" +str(n) for n in range(1,6)
+                    ]+['http://www.newsmth.net/nForum/section/ADAgents',
+                     'http://www.newsmth.net/nForum/section/Estate',
+                     'http://www.newsmth.net/nForum/section/SecondHand',
+                     'http://www.newsmth.net/nForum/section/Wealth']+[
+                    #二级菜单
+                    'http://www.newsmth.net/nForum/section/Association',
+                    'http://www.newsmth.net/nForum/section/THU',
+                    'http://www.newsmth.net/nForum/section/JokeWorld',
+                    'http://www.newsmth.net/nForum/section/Pictures',
+                    'http://www.newsmth.net/nForum/section/Stars',
+                    'http://www.newsmth.net/nForum/section/Basketball',
+                    'http://www.newsmth.net/nForum/section/FansClub',
+                    'http://www.newsmth.net/nForum/section/AbroadStudy',
+                    'http://www.newsmth.net/nForum/section/Job',
+                    'http://www.newsmth.net/nForum/section/Travels',
+                    'http://www.newsmth.net/nForum/section/Automobile',
+                    'http://www.newsmth.net/nForum/section/Coupons',
+                    'http://www.newsmth.net/nForum/section/Military',
+                    'http://www.newsmth.net/nForum/section/SmartLife',
+                    #终止版面，价值待定
+                    'http://www.newsmth.net/nForum/section/Closed',
+                    'http://www.newsmth.net/nForum/section/Disused',
+                    'http://www.newsmth.net/nForum/section/Merged',
+                    'http://www.newsmth.net/nForum/section/Removed',
+                    #频道推荐/电脑数码里面的二级菜单
+                    'http://www.newsmth.net/nForum/section/SmartLife']
 
     def parse(self, response):
         selector = Selector(response)
